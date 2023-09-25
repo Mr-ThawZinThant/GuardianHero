e076f89baad3a76accf8d020229254d40afec0cc
Thaw Zin Thant <thawzinthon@gmail.com>
2023-09-25 13:40:40 +0630

    fixed loading error

--- UI/Inventory/inventory.gd
+++ UI/Inventory/inventory.gd
@@ -3,6 +3,8 @@ class_name Inventory
 
 signal item_changed(indexes)
 
+var drag_data = null
+
 export (Array , Resource) var items = []
 
 func set_item(item_index , item):
@@ -16,9 +18,9 @@ func swap_item(item_index, target_item_index):
 	var item = items[item_index]
 	items[target_item_index] = item
 	items[item_index] = target_item
-	emit_signal("item_change" , [item_index , target_item_index])
+	emit_signal("item_changed" , [item_index , target_item_index])
 	
-func remove_item(item_index, item):
+func remove_item(item_index):
 	var previous_item = items[item_index]
 	items[item_index] = null
 	emit_signal("item_changed", [item_index])
